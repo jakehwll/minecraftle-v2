f8bda7d24ca0adec123eb7bc23bbe9ee3e0361cc
Jake <jake@hwll.me>
2024-07-10 19:14:59 +1000

    feat: add more content to `<GameOver />`

--- src/components/GameOver.tsx
+++ src/components/GameOver.tsx
@@ -1,12 +1,17 @@
 import useGameState from "../hooks/useGameState";
 import { Container } from "./Container";
+import classes from "./GameOver.module.css";
 
 export const GameOver = () => {
   const { gameState } = useGameState();
-  
+
+  const { craftingTables } = useGameState();
+
   return (
-    <Container>
+    <Container className={classes.root}>
       {gameState === "won" ? <h1>You win!</h1> : <h1>Game Over</h1>}
+      <p>You took {craftingTables.length} guesses!</p>
+      <p>Minecraftle {new Date().toISOString().split("T")[0]}</p>
     </Container>
   );
 };
