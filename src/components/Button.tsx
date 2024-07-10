82167aec3b23fd8ab64807bd5829b89e1c75fa4e
Jake <jake@hwll.me>
2024-07-10 19:34:19 +1000

    feat: implement basic button css (TODO)

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,9 @@
 import cc from "classcat";
 import { ComponentProps } from "preact/compat";
+import classes from "./Button.module.css";
 
 interface ButtonProps extends ComponentProps<"button"> {}
 
 export const Button = ({ className, ...props }: ButtonProps) => (
-  <button className={cc(className)} {...props} />
+  <button className={cc([classes.root, className])} {...props} />
 );
