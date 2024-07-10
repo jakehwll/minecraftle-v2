10844b91c0b61bd59d450578499b7ab367ab4a30
Jake <jake@hwll.me>
2024-07-10 13:21:20 +1000

    feat: disable if we dont have a `recipeResult`

--- src/components/Crafting.tsx
+++ src/components/Crafting.tsx
@@ -64,7 +64,7 @@ export const Crafting = ({
                 [classes.result__slot_disabled]: disabled,
               },
             ])}
-            onClick={() => onSubmit && onSubmit()}
+            onClick={() => onSubmit && recipeResult && onSubmit()}
           >
             {recipeResult && (
               <img
