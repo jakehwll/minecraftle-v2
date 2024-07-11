ce37048912c38653b779ee1dd01755aea2030a25
Jake <jake@hwll.me>
2024-07-11 12:59:03 +1000

    chore: add github link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,6 +7,11 @@ export const Footer = () => {
         Not an official Minecraft website. We are not associated with Mojang or
         Microsoft.
       </p>
+      <p>
+        <a href="https://github.com/jakehwll/minecraftle-v2">
+          GitHub
+        </a>
+      </p>
     </footer>
   );
 };
