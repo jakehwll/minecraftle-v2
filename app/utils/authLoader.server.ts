891004411bb4c4bf7b468dcb1c58fe5b122648ce
Jake <jake@hwll.me>
2024-07-25 13:00:53 +1000

    chore: cleanup legibility on `authLoader.server.ts`

--- app/utils/authLoader.server.ts
+++ app/utils/authLoader.server.ts
@@ -1,10 +1,11 @@
 import { lucia } from "./lucia";
 
-export const authLoader = async (request: Request) => {
-  const cookie = request.headers.get("Cookie");
-  if (!cookie) return { user: null, session: null };
-  const sessionId = lucia.readSessionCookie(cookie);
-  if (!sessionId) return { user: null, session: null };
-  const { user, session } = await lucia.validateSession(sessionId);
-  return { user, session };
+export const authLoader = async (req: Request) => {
+  const cookie = req.headers.get("Cookie");
+  const sessionId = cookie ? lucia.readSessionCookie(cookie) : null;
+  const session = sessionId ? await lucia.validateSession(sessionId) : null;
+
+  return {
+    user: session?.user,
+  };
 };
