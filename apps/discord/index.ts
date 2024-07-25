4f70a348a76172c82246de78a3c26cfd134f72c1
Jake <jake@hwll.me>
2024-07-25 21:17:40 +1000

    fix: use `discord-api-types` rather than `discord-interactions`

--- apps/discord/index.ts
+++ apps/discord/index.ts
@@ -1,36 +1,53 @@
 import { Hono } from "hono";
-import {
-  InteractionResponseType,
-  InteractionType,
-} from "discord-interactions";
 import { discordInteraction } from "./middleware/discordInteraction";
 import { logger } from "hono/logger";
+import {
+  InteractionType,
+  InteractionResponseType,
+  type APIInteractionResponseChannelMessageWithSource,
+} from "discord-api-types/v10";
 
 const app = new Hono();
 
 app.use("*", logger());
 app.use("*", discordInteraction);
 
-const COMMANDS = [
+declare global {
+  var hot: boolean;
+}
+
+const COMMANDS: {
+  name: string;
+  description: string;
+  callback: () => void;
+}[] = [
   {
     name: "help",
     description: "Replies with some help!",
+    callback: () => {
+      console.log("help");
+    },
   },
-]
+];
 
 const DEVELOPMENT_GUILD_ID = process.env.DISCORD_AUTH_BOT_GUILD_ID;
-const LOCAL_COMMANDS = `https://discord.com/api/v9/applications/${process.env.DISCORD_AUTH_CLIENT_ID}/guilds/${DEVELOPMENT_GUILD_ID}/commands`
-const GLOBAL_COMMANDS = `https://discord.com/api/v9/applications/${process.env.DISCORD_AUTH_CLIENT_ID}/commands`
+const LOCAL_COMMANDS = `https://discord.com/api/v9/applications/${process.env.DISCORD_AUTH_CLIENT_ID}/guilds/${DEVELOPMENT_GUILD_ID}/commands`;
+const GLOBAL_COMMANDS = `https://discord.com/api/v9/applications/${process.env.DISCORD_AUTH_CLIENT_ID}/commands`;
 
 const UPLOAD_COMMANDS = async () => {
-  await fetch(process.env.NODE_ENV === 'development' ? LOCAL_COMMANDS : GLOBAL_COMMANDS, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bot ${process.env.DISCORD_AUTH_BOT_TOKEN}`,
-    },
-    method: "PUT",
-    body: JSON.stringify(COMMANDS),
-  }).then((res) => {
+  await fetch(
+    process.env.NODE_ENV === "development" ? LOCAL_COMMANDS : GLOBAL_COMMANDS,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bot ${process.env.DISCORD_AUTH_BOT_TOKEN}`,
+      },
+      method: "PUT",
+      body: JSON.stringify(
+        COMMANDS.map(({ callback, ...rest }) => ({ ...rest }))
+      ),
+    }
+  ).then((res) => {
     res.status === 200
       ? console.log("Commands uploaded!", process.env.NODE_ENV)
       : console.log(
@@ -39,27 +56,31 @@ const UPLOAD_COMMANDS = async () => {
           process.env.NODE_ENV
         );
   });
-}
+};
 
-await UPLOAD_COMMANDS()
+if (!globalThis.hot) {
+  globalThis.hot ??= true;
+  await UPLOAD_COMMANDS();
+}
 
 app.post("/interaction", async (c) => {
   const message = await c.req.json();
 
-  if (message.type === InteractionType.PING) {
+  if (message.type === InteractionType.Ping) {
     return c.json({
-      type: InteractionResponseType.PONG,
+      type: InteractionResponseType.Pong,
     });
   } else {
-    return c.json({
-      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    const response: APIInteractionResponseChannelMessageWithSource = {
+      type: InteractionResponseType.ChannelMessageWithSource,
       data: {
         tts: false,
         content: "Hello, world!",
         embeds: [],
         allowed_mentions: { parse: [] },
       },
-    });
+    };
+    return c.json(response);
   }
 });
 
