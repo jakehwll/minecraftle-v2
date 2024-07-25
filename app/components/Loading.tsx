ea7270f53162b3f6b53aa6f1ba1a884523ef79e3
Jake <jake@hwll.me>
2024-07-25 12:37:55 +1000

    feat: rotating `...` on `<Loading />`

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,5 +1,18 @@
+import { useEffect, useState } from 'react';
 import classes from './Loading.module.css';
 
 export const Loading = () => {
-  return <div className={classes.root}>Loading...</div>;
+  const [dots, setDots] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((currentDots) =>
+        currentDots.length < 3 ? currentDots + "." : ""
+      );
+    }, 500); // Change dot every 500ms
+
+    return () => clearInterval(interval);
+  }, []);
+  
+  return <div className={classes.root}>Loading{dots}</div>;
 }
